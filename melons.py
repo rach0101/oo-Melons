d7b3cebc0b1f27e2c944201ef568dbc2739f8864
Rachel Cleak <rachelmcleak@gmail.com>
2021-07-24 16:33:37 -0700

    Completed Labpython3 -i melons.py

--- melons.py
+++ melons.py
@@ -11,11 +11,19 @@ class AbstractMelonOrder:
 
     def get_total(self):
         """Calculate price, including tax."""
-
         base_price = 5
+        
+        if self.species == 'Christmas Melon':
+            base_price = 1.5 * base_price
+
         total = (1 + self.tax) * self.qty * base_price
 
         return total
+
+    def mark_shipped(self):
+        """Record the fact than an order has been shipped."""
+
+        self.shipped = True
 
 
 class DomesticMelonOrder(AbstractMelonOrder):
@@ -26,10 +34,6 @@ class DomesticMelonOrder(AbstractMelonOrder):
         self.tax = 0.08
         super().__init__(species, qty)
 
-    def mark_shipped(self):
-        """Record the fact than an order has been shipped."""
-
-        self.shipped = True
 
 
 class InternationalMelonOrder(AbstractMelonOrder):
@@ -44,21 +48,38 @@ class InternationalMelonOrder(AbstractMelonOrder):
         self.tax = 0.17
         super().__init__(species, qty)
 
-
     def get_total(self):
         """Calculate price, including tax."""
+        flat_fee = 0
+        
+        if self.qty < 10:
+            flat_fee = 3
+        
+        return super().get_total() + flat_fee
 
-        base_price = 5
-        total = (1 + self.tax) * self.qty * base_price
-
-        return total
-
-    def mark_shipped(self):
-        """Record the fact than an order has been shipped."""
-
-        self.shipped = True
 
     def get_country_code(self):
         """Return the country code."""
 
         return self.country_code
+
+class GovernmentMelonOrder(AbstractMelonOrder):
+    """A melon order made by the government"""
+    
+    def __init__(self, species, qty, passed):
+        self.tax = 0
+        self.passed = passed
+
+        super().__init__(species, qty)
+
+
+    def mark_inspection(self):
+        """Takes boolean and updates whether or not melon has passed inspection"""
+        
+        passed_inspection = False
+
+        if self.passed:
+            print("YAAAAY I PASSED THE INSPECTION")
+            passed_inspection = True 
+        
+        return passed_inspection
